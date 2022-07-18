30416f01241ce9ee9b097238670c1e2ce713a2b5
deforde <daniel.forde001@gmail.com>
2022-07-18 07:55:21 +0200

    add a couple more tests

--- tests/test.sh
+++ tests/test.sh
@@ -53,11 +53,13 @@ assert 1 '1>=0;'
 assert 1 '1>=1;'
 assert 0 '1>=2;'
 
+assert 3 'a = 3;'
 assert 3 'a = 3; a;'
 assert 7 'a = 3; b = 4; a + b;'
 assert 14 'a = 3; b = 5 * 6 - 8; a + b / 2;'
 assert 17 'a = 4; b = 5 * (8 - 2); (a + b) / 2;'
 
+assert 3 'foo = 3;'
 assert 3 'foo = 3; foo;'
 assert 7 'foo = 3; bar = 4; foo + bar;'
 assert 14 'foo = 3; bar = 5 * 6 - 8; foo + bar / 2;'
